80842f0a96124f5ccae0a5d854cad40bb09d8241
doitkim <kimsh27214@gmail.com>
2022-06-13 14:25:43 +0900

    자바스크립트 1일차

---
+++ JavaScript/ch01/index.js
@@ -0,0 +1,16 @@
+// console.log('Hello Codeit');
+// console.log(10+5);
+// console.log(20-5);
+
+// 변수 선언
+
+// let espressoPrice = 3000;
+// let lattePrice = 4300;
+// let mochaPrice = 4800;
+
+// console.log(espressoPrice * 2);
+// console.log(lattePrice * 4 + mochaPrice * 2);
+// console.log(lattePrice + mochaPrice);
+// console.log(espressoPrice + lattePrice * 4 + mochaPrice * 2);
+// console.log(lattePrice * 3 );
+
