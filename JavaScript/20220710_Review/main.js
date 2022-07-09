85fb086649db540bf65d45ceac86bfcd3dc7f66b
doitkim <kimsh27214@gmail.com>
2022-07-10 07:29:51 +0900

    웹퍼블리셔 자바스크립트 초급 복습 완료

---
+++ JavaScript/20220710_Review/main.js
@@ -0,0 +1,43 @@
+// function logParticipant(name) {
+//     console.log(`${name}(이)가 대화에 참여했습니다.`)
+// }
+
+// logParticipant('동수');
+// logParticipant('윤하');
+// logParticipant('재준');
+// logParticipant('동훈');
+// logParticipant('영희');
+// logParticipant('신욱');
+
+// function expressMultiplication(num1, num2) {
+//     multy = num1 * num2;
+//     console.log(`${num1} * ${num2} = ${multy}`);
+// }
+
+// expressMultiplication(3, 4);
+// expressMultiplication(3, 2);
+// expressMultiplication(7, 5);
+// expressMultiplication(8, 9);
+// expressMultiplication(5, 5);
+// expressMultiplication(9, 9);
+
+// function calculateRectangleArea(width, height) {
+//     total = width * height;
+//     return total;
+// }
+
+// let area1 = calculateRectangleArea(3,4);
+// let area2 = calculateRectangleArea(8,5);
+// let area3 = calculateRectangleArea(7,2);
+
+// console.log(`Area1: ${area1}, Area2: ${area2}, Area3: ${area3}`);
+
+function orderSetMenu(sandwich, drink='스포트라이트') {
+    console.log(`주문하신 ${sandwich}, ${drink} 세트메뉴 나왔습니다!`);
+}
+
+orderSetMenu('코드웨잇 클럽');
+orderSetMenu('터키베이컨 아보카도', '코카콜라');
+orderSetMenu('코드웨잇 멜트');
+orderSetMenu('이탈리안 비엠티', '닥터페퍼');
+orderSetMenu('에그마요', '환타 오렌지');
