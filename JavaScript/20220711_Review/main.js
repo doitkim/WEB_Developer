4070285cff75a1e1d686bffa241c78a5b4c7c48e
doitkim <kimsh27214@gmail.com>
2022-07-12 22:46:12 +0900

    TPZ 리그 전적 갱신

--- JavaScript/20220711_Review/main.js
+++ JavaScript/20220711_Review/main.js
@@ -30,28 +30,80 @@
 // checkHeight(135);
 // checkHeight(170);
 
-function printGrade(midtermScore, finalScore){
-	let totalScore = midtermScore + finalScore;
+// function printGrade(midtermScore, finalScore){
+// 	let totalScore = midtermScore + finalScore;
 
-    if (totalScore >=90) {
-        console.log('A');
+//     if (totalScore >=90) {
+//         console.log('A');
+//     }
+//     else if (totalScore >= 80) {
+//         console.log('B');
+//     }
+//     else if (totalScore >= 70) {
+//         console.log('C');
+//     }
+//     else if (totalScore >= 60) {
+//         console.log('D');
+//     }
+//     else if (totalScore < 60) {
+//         console.log('F');
+//     }
+// }
+
+// // 테스트 코드
+// printGrade(25, 35);
+// printGrade(50, 45);
+// printGrade(29, 24);
+// printGrade(37, 42);
+
+// 나의 나이와, 나의 성별을 저장하는 변수
+let myAge = 26;
+let myGender = 'male';
+
+// 호칭을 담은 변수
+let callOlderBrother = '형';
+let callOlderSister = '누나';
+let callFriend = '친구';
+let callYoungerSister = '여동생';
+let callYoungerBrother = '남동생';
+
+// 상대방의 나이와 성별에 따른 호칭을 리턴하는 함수 whatShouldICall를 완성하세요.
+function whatShouldICallYou(yourAge, yourGender) {
+  if (myAge > yourAge) {
+    if (myGender == yourGender) {
+        // console.log(callYoungerBrother);
+        return callYoungerBrother;
     }
-    else if (totalScore >= 80) {
-        console.log('B');
+    else {
+        // console.log(callYoungerSister);
+        return callYoungerSister;
     }
-    else if (totalScore >= 70) {
-        console.log('C');
+  }
+  else if (myAge < yourAge) {
+    if (myGender == yourGender) {
+        // console.log(callOlderBrother);
+        return callOlderBrother;
     }
-    else if (totalScore >= 60) {
-        console.log('D');
+    else {
+        // console.log(callOlderSister);
+        return callOlderSister;
     }
-    else if (totalScore < 60) {
-        console.log('F');
-    }
+  }
+  else {
+    // console.log(callFriend);
+    return callFriend;
+  }
 }
 
 // 테스트 코드
-printGrade(25, 35);
-printGrade(50, 45);
-printGrade(29, 24);
-printGrade(37, 42);
+let result1 = whatShouldICallYou(25, 'female');
+let result2 = whatShouldICallYou(20, 'male');
+let result3 = whatShouldICallYou(26, 'female');
+let result4 = whatShouldICallYou(30, 'male');
+let result5 = whatShouldICallYou(31, 'female');
+
+console.log(result1);
+console.log(result2);
+console.log(result3);
+console.log(result4);
+console.log(result5);
