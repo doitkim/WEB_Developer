a92fc3ff0220c4a4069c630d77048729d7f9ce33
doitkim <kimsh27214@gmail.com>
2022-07-24 03:16:11 +0900

    TPZ 리그 갱신

---
+++ TPZ_League/TPZ_HOME.js
@@ -0,0 +1,7 @@
+$(function showPopup(){
+    window.open("./tonumant.html","토너먼트대진표","width=400, height=250, top=10, left=10");
+});
+
+$(function showPopup(){
+    window.open("./tonumant_Winners.html","우승준우승명단","width=220, height=420, top=450, left=850");
+});
